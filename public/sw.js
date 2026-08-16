44b1e78a1014215292f80f1f68cd2b7c01731024
Micka <mickael.aguillon@gmail.com>
2026-08-16 14:07:31 +0200

    SW : cache v2, météo modale détail par jour

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'myvibe-v1';
+const CACHE_NAME = 'myvibe-v2';
 const ASSETS = [
   '/', '/index.html', '/css/style.css',
   '/js/app.js', '/js/auth.js', '/js/widgets.js', '/js/modal.js',
